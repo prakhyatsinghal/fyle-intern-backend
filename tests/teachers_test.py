1f0f5a03584520844342c69bc73f5d1538481e33
Prakhyat Singhal <singhalprakhyat2001@gmail.com>
2024-01-18 22:08:45 +0530

    Added Missing APIs

--- tests/teachers_test.py
+++ tests/teachers_test.py
@@ -26,6 +26,24 @@ def test_get_assignments_teacher_2(client, h_teacher_2):
         assert assignment['state'] in ['SUBMITTED', 'GRADED']
 
 
+def test_grade_assignment_grade(client, h_teacher_1):
+    """
+    failure case: API should allow only grades available in enum
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_1,
+        json={
+            "id": 1,
+            "grade": "A"
+        }
+    )
+
+    assert response.status_code == 200
+    data = response.json['data']
+    assert data['grade'] == 'A'
+    
+
 def test_grade_assignment_cross(client, h_teacher_2):
     """
     failure case: assignment 1 was submitted to teacher 1 and not teacher 2
